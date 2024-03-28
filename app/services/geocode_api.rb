850d4aa53f7c7189e4911e33996d5fc00883af77
arianna haradon <ariannaharadon@gmail.com>
2024-03-28 13:13:25 -0400

    add error handling to geocode api

--- app/services/geocode_api.rb
+++ app/services/geocode_api.rb
@@ -8,7 +8,14 @@ class GeocodeApi
     query_params = URI.encode_www_form({address: address})
     url = "#{base_url}#{query_params}.json?access_token=#{access_token}"
     response = Net::HTTP.get_response(URI(url))
-    parse_geocode_response(response.body)
+    if response.is_a?(Net::HTTPSuccess)
+      parse_geocode_response(response.body)
+    else
+      puts "API request failed with status code: #{response.code}"
+      nil
+    end
+  rescue => e
+    puts "Error fetching geocoded data: #{e.message}"
   end
 
   def self.parse_geocode_response(response)
