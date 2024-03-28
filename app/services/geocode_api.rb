244fa7d2841a99860d42213c7a277908f4c37384
arianna haradon <ariannaharadon@gmail.com>
2024-03-28 10:30:26 -0400

    add geocode_api

--- app/services/geocode_api.rb
+++ app/services/geocode_api.rb
@@ -1,5 +1,18 @@
-class GeocodeAPI
+require 'net/http'
+require 'json'
+
+class GeocodeApi
   def self.geocode(address)
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token"
+    access_token = ENV.fetch('MAPBOX_TOKEN')
+    base_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+    query_params = URI.encode_www_form({address: address})
+    url = "#{base_url}#{query_params}.json?access_token=#{access_token}"
+    response = Net::HTTP.get_response(URI(url))
+    parse_geocode_response(response.body)
   end
+
+  def self.parse_geocode_response(response)
+    parsed_data = JSON.parse(response)
+    parsed_data['features'][0]['geometry']['coordinates']
+  end 
 end
